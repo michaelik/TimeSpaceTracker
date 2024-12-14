5d87225d596c0c1a8cf02d6fcdd3a579b6dae04e
michaelik <mikeikechi3@gmail.com>
2024-12-14 10:42:14 +0100

    feat: integrate Swagger for API documentation
    - Added `springdoc-openapi-starter-webmvc-ui` dependency for Swagger support.
    - Configured Swagger with a custom OpenAPI bean.
    - The Swagger UI is accessible at `/swagger-ui.html`.
    
    Signed-off-by: michaelik <mikeikechi3@gmail.com>

---
+++ src/main/java/com/example/config/OpenApiConfig.java
@@ -0,0 +1,23 @@
+package com.example.config;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI usersMicroserviceOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Algorithm Complexity Analyzer API")
+                        .description("REST API for analyzing algorithm complexity")
+                        .version("v1.0")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("GitHub Repository")
+                        .url("https://github.com/example/repository"));
+    }
+}
